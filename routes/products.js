fb4b9f132d72192b2da8965dfa221207033d33cd
Deka-KEAN <anupdeka698@gmail.com>
2023-10-10 22:53:35 +0530

    Getting Products By Category

--- routes/products.js
+++ routes/products.js
@@ -6,4 +6,20 @@ router.get("/products", (req, res) => {
   res.send(Products);
 });
 
+
+router.get('/products/categories/:category',(req,res)=>{
+  const category=req.params.category;
+  let result=[];
+  Products.map(a => {
+    if(a.category===category)
+      return result.push(a);
+  });
+  if(result.length !== 0){
+    res.send(result);
+  }else{
+    res.status(404).send("Category Not Found");
+  }
+})
+
+
 module.exports = router;
