d2789ddf936bc5bdafaa92b58f90ea43804f7a5c
Varad Dhumale <varaddhumale177@gmail.com>
2023-10-10 22:30:19 +0530

    the end point named as /users/search?zipcode={zipcode} is added

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
 const productsRouter = require("./routes/products");
+const userRouter = require("./routes/user");
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.use("/", productsRouter);
+app.use("/", userRouter);
 
 app.listen(port, () => console.log(`http://localhost:${port}`));
