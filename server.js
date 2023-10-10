a0529bb8a5505c5be9e529b00b363f05efac0600
Priyadarshini <priyadarshini.roy03@gmail.com>
2023-10-10 22:47:58 +0530

    search user by username route added

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
 const productsRouter = require("./routes/products");
+const usersRouter = require("./routes/users");
 
 app.get("/", (req, res) => res.send("Hello World!"));
-app.use("/", productsRouter);
+app.use("/products", productsRouter);
+app.use("/users", usersRouter);
 
 app.listen(port, () => console.log(`http://localhost:${port}`));
