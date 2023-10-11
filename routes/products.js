937cc8401bc7c394847332709db76487917bb4d3
Honey2339 <prasoon2study@gmail.com>
2023-10-11 15:43:35 +0530

    Added Feature #6 Created endpoint to get all categories

--- routes/products.js
+++ routes/products.js
@@ -5,5 +5,10 @@ const Products = require("../Products.json");
 router.get("/products", (req, res) => {
   res.send(Products);
 });
+router.get("/products/category", async (req, res) => {
+  const categories = Products.map((product) => product.category);
+  const allCategories = [...new Set(categories)];
+  res.send(allCategories);
+});
 
 module.exports = router;
