8d2585526a2b91312961488b318bdb27b90fe736
OM1705 <ompol1705@gmail.com>
2023-10-12 10:58:35 +0530

    Added a new route to search products title

--- routes/products.js
+++ routes/products.js
@@ -5,10 +5,24 @@ const Products = require("../Products.json");
 router.get("/", (req, res) => {
   res.send(Products);
 });
+
 router.get("/category", async (req, res) => {
   const categories = Products.map((product) => product.category);
   const allCategories = [...new Set(categories)];
   res.send(allCategories);
+});
+
+router.get("/search", (req, res) => {
+  const searchTerm = req.query.title;
+  if (!searchTerm) {
+    return res.status(400).json({ error: "Title parameter is required." });
+  }
+
+  const matchingProducts = Products.filter((product) => {
+    return product.title.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
+  res.json(matchingProducts);
 });
 
 module.exports = router;
