f620a325b9e4363e65d042bdea9a29fcf70caa5e
Varad Dhumale <varaddhumale177@gmail.com>
2023-10-12 23:23:49 +0530

    getting the product by range  - task done

--- routes/products.js
+++ routes/products.js
@@ -2,13 +2,32 @@ const express = require("express");
 const router = express.Router();
 const Products = require("../Products.json");
 
-router.get("/", (req, res) => {
-  res.send(Products);
-});
+// router.get("/", (req, res) => {
+//   res.send(Products);
+// });
 router.get("/category", async (req, res) => {
   const categories = Products.map((product) => product.category);
   const allCategories = [...new Set(categories)];
   res.send(allCategories);
+});
+
+
+
+router.get("/", (req, res) => {
+  const minPrice = parseFloat(req.query.minPrice);
+  const maxPrice = parseFloat(req.query.maxPrice);
+
+  if (minPrice  && maxPrice) {
+    const filteredProducts = Products.filter((product) => {
+      const productPrice = parseFloat(product.price);
+      return productPrice >= minPrice && productPrice <= maxPrice;
+    });
+
+    console.log(filteredProducts.length);
+    res.json(filteredProducts);
+  } else {
+    res.send(Products);
+  }
 });
 
 module.exports = router;
