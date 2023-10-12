2751bf3dc3b89d45bf7011d532c54cc31ed7f1a3
Varad Dhumale <varaddhumale177@gmail.com>
2023-10-12 23:00:57 +0530

    getting user by zipcode - updated

--- routes/users.js
+++ routes/users.js
@@ -2,40 +2,26 @@ const express = require("express");
 const router = express.Router();
 const Users = require("../Users.json");
 
-router.get("/" , (req,res) => {
-    res.send(Users);
-})
-
+router.get("/", (req, res) => {
+  res.send(Users);
+});
 
 router.get("/search", (req, res) => {
-  const { zipcode } = req.query;
-  if (!zipcode) {
-    return res.status(400).json({ error: "Zip code is required." });
+  const { zipcode, username } = req.query;
+
+  let filteredUsers = Users;
+
+  if (username) {
+    filteredUsers = filteredUsers.filter((user) => user.username === username);
   }
-  // Search for users with matching zip code
-  const filteredUsers = Users.filter(
-    (user) => user.address.zipcode === zipcode
-  );
-  if (filteredUsers.length === 0) {
-    return res
-      .status(404)
-      .json({ error: "No users found with that zip code." });
+
+  if (zipcode) {
+    filteredUsers = filteredUsers.filter(
+      (user) => user.address.zipcode === zipcode
+    );
   }
+
   res.json(filteredUsers);
 });
-
-router.get("/search" , (req,res) => {
-    const {username} = req.query;
-
-    if(!username) return res.send(Users);
-
-    const user = Users.filter((user) => user.username === username);
-    
-    res.send(user);
-});
-
-
-
-
 
 module.exports = router;
