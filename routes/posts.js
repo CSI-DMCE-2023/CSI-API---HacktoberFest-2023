2435a2d22d4895ffb5328b2a73a5da55f070fa47
Priyadarshini <priyadarshini.roy03@gmail.com>
2023-10-19 11:40:12 +0530

    pagination added to posts

--- routes/posts.js
+++ routes/posts.js
@@ -3,34 +3,40 @@ const router = express.Router();
 const Posts = require("../posts.json");
 
 router.get("/search/:userId", (req, res) => {
-  const userId  = parseInt(req.params.userId);
+  const userId = parseInt(req.params.userId);
   let results = [];
   Posts.posts.map((post) => {
-    if(post.userId === userId)results.push(post);
+    if (post.userId === userId) results.push(post);
   });
   if (results.length !== 0) {
     res.send(results);
   } else {
     res.status(404).send("No Product found");
   }
-})
+});
 
-router.get('/tag/:tag', (req, res) => {
+router.get("/tag/:tag", (req, res) => {
   const { tag } = req.params;
+  const page = req.query.page || 1;
+  const pageSize = 5;
 
   try {
-    // Filter posts that contain the specified tag
-    const filteredPosts = Posts.posts.filter(post => post.tags.includes(tag));
+    const filteredPosts = Posts.posts.filter((post) => post.tags.includes(tag));
 
     if (filteredPosts.length === 0) {
-      // No posts with the specified tag found, return a 404 response
       res.status(404).json({ error: "No posts with the specified tag found" });
     } else {
-      // Posts with the specified tag found, return them
-      res.json({ posts: filteredPosts });
+      const startIndex = (page - 1) * pageSize;
+      const endIndex = page * pageSize;
+      const paginatedPosts = filteredPosts.slice(startIndex, endIndex);
+
+      res.json({
+        totalPages: Number((filteredPosts.length / pageSize).toFixed(0)),
+        currentPage: Number(page),
+        posts: paginatedPosts,
+      });
     }
   } catch (err) {
-    // Handle any unexpected errors
     console.error(err);
     res.status(500).json({ error: "Internal server error" });
   }
