c8249435e08f788af8b26655c5a80fd6ecf1e700
OM1705 <ompol1705@gmail.com>
2023-10-20 22:39:58 +0530

    Added pagination for Users router

--- routes/users.js
+++ routes/users.js
@@ -20,4 +20,16 @@ router.get("/search", (req, res) => {
   res.json(filteredUsers);
 });
 
+router.get("/paginate", (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const pageSize = parseInt(req.query.pageSize) || 10;
+
+  const startIndex = (page - 1) * pageSize;
+  const endIndex = page * pageSize;
+
+  const usersOnPage = Users.slice(startIndex, endIndex);
+
+  res.json(usersOnPage);
+});
+
 module.exports = router;
